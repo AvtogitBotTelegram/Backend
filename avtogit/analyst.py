c17583cca2594377c491d592e7fabd99efe27c9f
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-07-24 12:36:07 +0800

    feature: model abalyst and handl expireired-details

---
+++ avtogit/analyst.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+import pandas
+
+from avtogit.schemas import ModelDetails
+
+
+class Analyst:
+
+    def expired_details(self, bytes_file: bytes) -> list[ModelDetails]:
+        file = pandas.read_excel(bytes_file)
+        file['Ожидаемый срок'] = pandas.to_datetime(file['Ожидаемый срок'], format='%d.%m.%Y %H:%M')
+        details = file[
+            (file['Готово к выдаче, шт'].notna()) &
+            ((file['Ожидаемый срок'] + timedelta(days=15)) < datetime.now()) &
+            (file['Цена в закупке, ₽'] > 1000)
+        ]
+        bad_details = []
+        for detail in details.itertuples():
+            bad_details.append(
+                ModelDetails(
+                    customer_name=detail[5],
+                    detail_number=detail[7],
+                    detail_name=detail[8],
+                    detail_price=detail[14],
+                    expected_time=detail[20].to_pydatetime(),
+                )
+            )
+        return [ModelDetails.from_orm(detail) for detail in bad_details]
