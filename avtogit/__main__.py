f5b8253754fb14408b9560af62f2a514fb269102
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-07-24 13:38:21 +0800

    reature: add handle app errors

--- avtogit/__main__.py
+++ avtogit/__main__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
+from avtogit.errors import AppError
 from avtogit.views import detail
+
+
+def handle_app_errors(error: AppError):
+    return {'error': error.reason}, error.status
+
 
 app = Flask(__name__)
 
 app.register_blueprint(detail.detail, url_prefix='/api/v1/detail/')
+
+app.register_error_handler(AppError, handle_app_errors)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
