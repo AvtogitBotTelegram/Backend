277981c2fa65296c8ec10aeaa0b2a6720d2d6550
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-08-05 17:34:34 +0800

    reature: add heandle

--- avtogit/__main__.py
+++ avtogit/__main__.py
@@ -11,6 +11,7 @@ def handle_app_errors(error: AppError):
 app = Flask(__name__)
 
 app.register_blueprint(detail.detail, url_prefix='/api/v1/detail/upload')
+app.register_blueprint(detail.orders, url_prefix='/api/v1/orders/upload')
 
 app.register_error_handler(AppError, handle_app_errors)
 
--- avtogit/views/detail.py
+++ avtogit/views/detail.py
@@ -6,6 +6,7 @@ from avtogit.analyst import Analyst
 from avtogit.schemas import ModelDetails
 
 detail = Blueprint('detail', __name__)
+orders = Blueprint('orders', __name__)
 
 analyst = Analyst()
 
@@ -23,3 +24,9 @@ def url():
     bytes_file = httpx.get(url_file).content
     bad_details = analyst.expired_details(bytes_file)
     return orjson.dumps([ModelDetails.from_orm(entity).dict() for entity in bad_details])
+
+
+@orders.post('/')
+def send_orders():
+    orders = orjson.loads(request.data)
+    return ('True', 200)
